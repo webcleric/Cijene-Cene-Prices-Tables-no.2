73a15658c1490157ccb0416dbfb4cb614ed7f217
DESKTOP-V6CKKJH\Nikola Savic <nikolasavic@outlook.com>
2017-11-17 21:54:12 +0100

    Fixing form

---
+++ cestitamo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Cestitamo!</title>
+</head>
+<body>
+	<h1>Poslali ste prijavu, hvala.</h1>
+	<h2>Yippi yay!</h2>
+</body>
+</html>
--- forma.php
+++ forma.php
@@ -1,13 +1,38 @@
 <?php
-    // Metoda post
-    $name = $_POST ('imeiprezime');
-    $townandcity = $_POST ('gradidrzava');
-    $email = $_POST ('email');
-    $phone = $_POST ('telefon');
-    // Izvuci podatke
-    echo $name;
-    echo $townandcity;
+    // izvuce informacije
+    $imeiprezime = $_POST ['imeiprezime'];
+    $gradidrzava = $_POST ['gradidrzava'];
+    $email = $_POST ['email'];
+    $kolikodugo = $_POST ['kolikodugo'];
+    $ocekivanja = $_POST ['ocekivanja'];
+    $telefon = $_POST ['telefon'];
+    $web = $_POST ['web'];
+    $oprema = $_POST ['oprema'];
+    $izaberi = $_POST ['izaberi'];
+    // test
+    echo $imeiprezime;
+    echo $gradidrzava;
     echo $email;
-    echo $phone;
-    // Procesuj informacije i posalji ih
+    echo $kolikodugo;
+    echo $ocekivanja;
+    echo $telefon;
+    echo $web;
+    echo $oprema;
+    echo $izaberi;
+    // Procesuj izvucene informacije i posalje ih
+    $email_poruka = "
+    
+    Sa ili bez smeštaja: ".$izaberi."
+    Ime i Prezime: ".$imeiprezime."
+    Grad i Drzava: ".$gradidrzava."
+    E-mail: ".$email."
+    Telefon: ".$telefon."
+    Web: ".$web."
+    Koliko dugo se bavite fotografijom: ".$kolikodugo."
+    Šta očekujete od ove radionice: ".$ocekivanja."
+    Koju opremu (fotoaparat) koristite za rad: ".$oprema."
+    
+    ";
+    mail { "nesto@nesto.com" , "Kontakt forma sa Cena" , $izaberi_imeiprezime_gradidrzava_email_telefon_web_kolikodugo_ocekivanja_oprema };
+    header {"Location: cestitamo.php"};
  ?>

d4b2ab3efdc43e5ab5d46e321350aea39efdfe3e
DESKTOP-V6CKKJH\Nikola Savic <nikolasavic@outlook.com>
2017-11-19 02:43:22 +0100

    FIXED!!!

--- formica.php
+++ formica.php
@@ -1,7 +1,5 @@
 <?php
-    $mail_to = 'alokinsavic@gmail.com'; // Specifiše email
-
-    // Izvule podatke iz HTML
+    // Izvuče podatke iz HTML
     $imeiprezime = $_POST['imeiprezime'];
     $gradidrzava = $_POST['gradidrzava'];
     $email = $_POST['email'];
@@ -11,6 +9,12 @@
     $web = $_POST['web'];
     $oprema = $_POST['oprema'];
     $izaberi = $_POST['izaberi'];
+
+
+    // Konstruiše naslov pošte i pošalje
+    
+    $mail_to = 'nikolasavic@outlook.com'; // Specifiše email
+    $subject = 'Poslano sa kontakt forme na luxworkshops.co/cene od: ' . $imeiprezime;
     
     // Ispise informacije (Test Environment)
 
@@ -26,37 +30,49 @@
     echo $izaberi;
     */
 
-    // Konstruiše naslov pošte
-    $subject = 'www.mysite.com Message from visitor ' . $imeiprezime;
-
     // Konstruiše sadržaj
-    $body_message = 'Ime i prezime: ' . $imeiprezime . "\r\n";
-    $body_message = 'Grad i država: ' . $gradidrzava . "\r\n";
-    $body_message = 'E-mail: ' . $email . "\r\n";
-    $body_message = 'Koliko dugo se aktivno bavite fotografijom:: ' . $kolikodugo . "\r\n";
-    $body_message = 'Očekivanja: ' . $ocekivanja . "\r\n";
-    $body_message = 'Kontakt Telefon: ' . $telefon . "\r\n";
-    $body_message = 'Web Lokacija: ' . $web . "\r\n";
-    $body_message = 'Koju opremu (fotoaparat) koristite za rad:: ' . $oprema . "\r\n";
-    $body_message = 'Sa ili bez smeštaja:: ' . $izaberi . "\r\n";
+    $body_message = 'Ime i prezime: '.$imeiprezime."\n";
+    $body_message = 'Grad i Država: '.$gradidrzava."\n";
+    $body_message .= 'E-mail: '.$email."\n";
+    $body_message .= 'Koliko dugo se aktivno bavite fotografijom: '.$kolikodugo."\n";
+    $body_message .= 'Očekivanja: '.$ocekivanja."\n";
+    $body_message .= 'Kontakt Telefon: '.$telefon."\n";
+    $body_message .= 'Web Lokacija: '.$web."\n";
+    $body_message .= 'Koju opremu (fotoaparat) koristite za rad: '.$oprema."\n";
+    $body_message .= 'Sa ili bez smeštaja:: ' . $izaberi;
+
+    /*
+    $imeiprezime = 'Ime i prezime: ' . $imeiprezime . "\r\n";
+    $gradidrzava = 'Grad i država: ' . $gradidrzava . "\r\n";
+    $email = 'E-mail: ' . $email . "\r\n";
+    $kolikodugo = 'Koliko dugo se aktivno bavite fotografijom:: ' . $kolikodugo . "\r\n";
+    $ocekivanja = 'Očekivanja: ' . $ocekivanja . "\r\n";
+    $telefon = 'Kontakt Telefon: ' . $telefon . "\r\n";
+    $web = 'Web Lokacija: ' . $web . "\r\n";
+    $oprema = 'Koju opremu (fotoaparat) koristite za rad:: ' . $oprema . "\r\n";
+    $izaberi = 'Sa ili bez smeštaja:: ' . $izaberi . "\r\n";
+    */
     
 
     // Konstruiše Zaglavlje
-    $headers = 'From: ' . $mail_from . "\r\n";
-    $headers .= 'Reply-To: ' . $mail_from . "\r\n";
+    $headers = 'From: ' . $imeiprezime . "\r\n";
+    $headers .= 'Reply-To: ' . $email . "\r\n";
 
-    $mail_sent = mail($mail_to, $subject, $body_message, $headers);
+    $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
-    if ($mail_sent == true){ ?>
-        <script language="javascript" type="text/javascript">
-        alert('Hvala na prijavi. Kontaktirat ćemo vas uskoro.');
-        window.location = 'http://www.luxworkshops.co';
+
+        if ($mail_status) { ?>
+      <script language="javascript" type="text/javascript">
+            alert('Hvala na prijavi. Kontaktirat ćemo vas uskoro.');
+            document.location = 'http://www.luxworkshops.co'
         </script>
-    <?php } else { ?>
-    <script language="javascript" type="text/javascript">
-        alert('Poruka Nije poslata. Molimo vas da se javite na  leon@leonbijelic.com ili nas nazvati na : +387 65 49 13 13 (Leon)');
-        window.location = 'http://www.luxworkshops.co';
-    </script>
     <?php
     }
-?>
+    else { ?>
+        <script language="javascript" type="text/javascript">
+            alert('Poruka Nije poslata. Molimo vas da se javite na  leon@leonbijelic.com ili nas nazvati na : +387 65 49 13 13 (Leon)');
+            document.location = 'http://www.luxworkshops.co'
+        </script>
+    <?php
+    }
+    ?>
